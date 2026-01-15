af94c51f644212a43fed567a0c5fc8b30f20b5fa
Slices1 <chocolatecroissant9@gmail.com>
2026-01-15 15:23:37 +0000

    readme added

---
+++ README.md
@@ -0,0 +1,6 @@
+![screenshot of ball pit](readme_screenshot1.png)
+![screenshot of ball pit](readme_screenshot2.png)
+
+accurate collisions, using general solution to the simulataneous equations
+- e = (v2 - v1)/(u1 - u2)
+- m1\*u1 + m2\*u2 = m1\*v1 + m2\*v2
--- src/main-brownian-motion-ver.rs
+++ src/main-brownian-motion-ver.rs
@@ -1,4 +1,7 @@
+#![feature(get_many_mut)]
 use macroquad::prelude::*;
+use rayon::prelude::*;
+
 #[derive(Clone)]
 struct Ball {
     radius: f32,
@@ -10,7 +13,7 @@ struct Ball {
 fn spawn_ball(position: Vec2, balls: &mut Vec<Ball>) {
     
     balls.push(Ball {
-        radius: rand::gen_range(15., 30.),
+        radius: rand::gen_range(1.5, 1.5),
         position: position,
         velocity: Vec2{x:0.,y:0.},
         colour: WHITE,
@@ -35,6 +38,7 @@ fn ball_to_ball_collision(ball_1: &mut Ball, ball_2: &mut Ball, c: f32) {
     let mass1 = (ball_1.radius).powi(2);
     let mass2 = (ball_2.radius).powi(2);
     let line_of_centres: Vec2 = normalise(ball_2.position - ball_1.position);
+
     let u1_perp: Vec2;
     let u1_paral: Vec2;
     let u2_perp: Vec2;
@@ -73,59 +77,50 @@ pub fn random_pastel() -> Color {
 
 #[macroquad::main("Ball Pit Sim in Rust")]
 async fn main() {
-    // set_fullscreen(true);
-    // for _i in 1..=20 { // so full screen has time to open
-    //     clear_background(GRAY);
-    //     next_frame().await;
-    // }
+    set_fullscreen(true);
+    for _i in 1..=20 { // so full screen has time to open
+        clear_background(GRAY);
+        next_frame().await;
+    }
 
     let mut balls: Vec<Ball> = Vec::new();
     
-    let gravity: f32 = 0.01;
-    let ball_to_wall_c: f32 = 0.90;
-    let ball_to_ball_c: f32 = 0.80;
+    let gravity: f32 = 0.0;
+    let ball_to_wall_c: f32 = 0.8;
+    let ball_to_ball_c: f32 = 0.8;
 
     // Spawning method #1 - all at once
-    // for i in 1..=12 {
-    //     for j in 1..=10 {
-    //         spawn_ball(Vec2{x:30. + 40.*(i as f32),y:screen_height() -30. - 22.*(j as f32)}, &mut balls);
-    //     }
-    // }
-
-    //balls[24].position.x += 1.;
-
-    // spawning method #3 - grid, every 50 pixels
-    let spacing: f32 = 30.;
-    let x_count = (screen_width()/spacing) as i32 - 1;
-    let y_count = (screen_height()/spacing) as i32 - 1;
-    for i in 1..x_count {
-        for j in 1..y_count {
-            spawn_ball(Vec2{x:spacing/2. + spacing*(i as f32),y:spacing/2. + spacing*(j as f32)}, &mut balls);
+    for i in 1..=80 {
+        for j in 1..=40 {
+            spawn_ball(Vec2{x:30. + 10.*(i as f32),y:screen_height() -30. - 10.*(j as f32)}, &mut balls);
         }
     }
 
+    //balls[24].position.x += 1.;
 
     // colour them
     for ball in &mut balls {
         ball.colour = random_pastel();
+        ball.velocity = Vec2{x:rand::gen_range(-70., 70.),y:rand::gen_range(-70., 70.)};
     }
 
     let mut frame_number: u64 = 0;
     let mut ball_count = balls.len();
     loop {
         clear_background(GRAY);
-        // spawn
+        //spawn
         // Spawning method #2 - horizontal sinusoidal motion
         // frame_number+=1;
-        // if frame_number % 16 == 0 {
+        // if frame_number % 4 == 0 {
         //     spawn_ball(Vec2{x:screen_width()*((frame_number as f32/50.).sin()+1.)/2.,y:100.}, &mut balls);
         //     ball_count += 1;
         // }
-        if is_mouse_button_down(MouseButton::Left) {
-        // if (is_mouse_button_pressed(MouseButton::Left)) {
+
+        if (is_mouse_button_pressed(MouseButton::Left)) {
             spawn_ball(mouse_position().into(), &mut balls);
             ball_count += 1;
             balls[ball_count-1].colour = random_pastel();
+            balls[ball_count-1].radius = 40.;
         }
 
         //update positions
@@ -172,21 +167,19 @@ async fn main() {
 
 
             for (i, j) in index_pairs {
-                // We only need to check collisions and resolve them.
-                // We split the slice at index j.
-                // 'head' contains indices [0..j], which includes 'i' because i < j
-                // 'tail' contains indices [j..len], so tail[0] is effectively balls[j]
-                let (head, tail) = balls.split_at_mut(j);
-                
-                let ball_1 = &mut head[i];
-                let ball_2 = &mut tail[0];
-
-                if check_collides(ball_1, ball_2) {
-                    ball_to_ball_collision(ball_1, ball_2, ball_to_ball_c);
+                if check_collides(&balls[i], &balls[j]) { // note: can pass these by copy too
+                    
+                    match balls.get_many_mut([i, j]) {
+                        Ok([ball_1, ball_2]) => ball_to_ball_collision(ball_1, ball_2, ball_to_ball_c),
+                        Err(_err) => {
+                            // Index i = index j; in this case no collision occurs!
+                        }
+                    }
                 }
             }
+
         //draw
-        for ball in &balls {
+        for ball in balls.clone().into_iter() {
             draw_circle(ball.position.x, ball.position.y, ball.radius, ball.colour);
         }
         next_frame().await
